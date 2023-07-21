8bf2eceb6eb88a79407b0dd58254a970c6a9ac31
DhanushRedy24 <reddydhanushreddy2424@gmail.com>
2023-07-21 10:48:03 +0530

    first commit

---
+++ commands.sh
@@ -0,0 +1,31 @@
+source /Users/dhanush/Desktop/firstAPP/firstAPP_env/bin/activate
+nvm use 20.2.0
+npm run build
+npm start
+npm install <name>
+npx create-react-app <my-app>
+
+
+cd /Users/dhanush/Desktop/firstAPP/firstAPP-new/firstapp_api
+cd /Users/dhanush/Desktop/firstAPP/firstAPP-new/firstapp_ui
+
+pip freeze > requirements.txt 
+pip install -r requirements.txt
+
+git status
+git add .
+git commit -m 'commit'
+git push -u origin main
+git pull origin main
+git clone <http url>
+git remote add origin https://github.com/DhanushReddy24/firstAPP-new.git
+
+django-admin startproject <projectname>
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
+python manage.py runserver 0:8000
+
+lsof -i :3000
+kill 12345
+kill -9 12345
