ce06be8f5d4248820711a6204c671fc65ad608e4
DhanushRedy24 <reddydhanushreddy2424@gmail.com>
2024-01-27 19:17:46 +0530

    general

--- firstapp_api/firstapp_api/settings.py
+++ firstapp_api/firstapp_api/settings.py
@@ -34,7 +34,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://hooksup.vercel.app',
-    'https://hooksup-api.vercel.app/
+    'https://hooksup-api.vercel.app/',
 ]
 
 # Application definition
