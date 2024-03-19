bb615e5a469af9e18abff4bc2bad6c88bb7ad3c6
Ravikumar Malleboina <ravikumar.malleboina@subitup.com>
2024-03-19 18:57:24 +0530

    #made changes in the Welcome component

--- firstapp_ui/src/authentication/Welcome.js
+++ firstapp_ui/src/authentication/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Welcome() {
     return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>This is the home page</h1>
       <a href="/login/" class="ml-2">Login</a><br />
       <a href="/sample_1/" class="ml-2">Sample_1</a>
     </div>
